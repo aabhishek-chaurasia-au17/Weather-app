d210344ef47da0fa091cbef568e9d62b070ab978
Abhishek <abhishekc838@gmail.com>
2022-01-04 09:24:28 +0530

    weather ⛈ app with geolocation 🗺 information

--- index.js
+++ index.js
@@ -37,6 +37,63 @@ const weather = {
     }
 }
 
+
+const geocode = {
+    reverseGeode(latitude, longitude){
+        var api_key = '9ba142b66ac04a34910563448f7f312a';
+        
+        var api_url = 'https://api.opencagedata.com/geocode/v1/json'
+
+        var request_url = api_url + '?' + 'key=' + api_key + '&q=' + encodeURIComponent(latitude + ',' + longitude) + '&pretty=1' + '&no_annotations=1';
+
+        // see full list of required and optional parameters:
+        // https://opencagedata.com/api#forward
+
+        var request = new XMLHttpRequest();
+        request.open('GET', request_url, true);
+
+        request.onload = function() {
+        // see full list of possible response codes:
+        // https://opencagedata.com/api#codes
+
+            if (request.status === 200){ 
+                // Success!
+                var data = JSON.parse(request.responseText);
+                // console.log(data.results[0]);
+                weather.fetchWeather(data.results[0].components.state)
+            } else if (request.status <= 500){ 
+                // We reached our target server, but it returned an error
+                                    
+                console.log("unable to geocode! Response code: " + request.status);
+                var data = JSON.parse(request.responseText);
+                console.log('error msg: ' + data.status.message);
+            } else {
+                console.log("server error");
+            }
+        };
+
+        request.onerror = function() {
+        // There was a connection error of some sort
+        console.log("unable to connect to server");        
+        };
+
+        request.send();  // make the request
+
+    },
+
+    getLocation(){
+        function success(data){
+            geocode.reverseGeode(data.coords.latitude, data.coords.longitude)
+        }
+        if(navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(success, console.error)
+        }else{
+            weather.fetchWeather('Bengaluru')
+        }
+    }
+}
+
+
 function onClickSearchBtn() {
     weather.search();
 }
@@ -50,5 +107,6 @@ function onInputEnter() {
     }
 }
 
-weather.fetchWeather('Delhi')
+
+geocode.getLocation()
 
