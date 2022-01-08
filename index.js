df43d8c0557e5c5f1dcf98a1b3cb1cbcb3eaafc3
Abhishek <abhishekc838@gmail.com>
2022-01-08 21:08:35 +0530

    movie seat booking app

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ const weather = {
     fetchWeather(city) {
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apiKey}`)
         .then((response) => response.json())
-        .then((data) => this.displayWeather(data));
+        .then((data) => this.displayWeather(data))
+        .catch((error) => console.log(error))
     },
     
     displayWeather(data) {
